a022751c0d544bde7620978385287e46016c93f6
Minhaj Ahammed <minhaj.mec94@gmail.com>
2026-06-22 01:54:00 +0530

    Removing debug flag

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def get_deployment(deployment_id):
     return jsonify(deployment.to_dict()), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000)
